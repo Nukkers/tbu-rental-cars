0b61754beacb03434ae8173a10678ffa80431e14
Nukkers <naukhez.3@hotmail.co.uk>
2022-05-16 18:42:47 +0100

    got basics of suggestion list working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import searchIcon from './search.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      if (text.length > 1) {
+        try {
+          const response = await fetch(
+            `https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${text}`
+          );
+          if (!response.ok) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          let actualData = await response.json();
+          setData(actualData.results.docs);
+        } catch (err) {
+          console.log('error:', err);
+        }
+      }
+    };
+    getData();
+  }, [text]);
+
+  function onChangeHandler(text) {
+    setText(text);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header container-fluid">
+      <form className="bg-warning p-3 rounded" role="search">
+        <input
+          onChange={(e) => onChangeHandler(e.target.value)}
+          value={text}
+          className="col-md-12"
+          type="text"
+          placeholder="Pick-up location"
+          aria-label="Pick-up location"
+          aria-required="true"
+        />
+        {data &&
+          text.length > 1 &&
+          data.map((item, key) => (
+            <div key={key} className="">
+              {item.name}
+            </div>
+          ))}
+      </form>
     </div>
   );
 }
-
 export default App;
